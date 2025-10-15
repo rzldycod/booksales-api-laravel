10594113e122274e5ae8a76ded372e235b51e3f7
Rizaldy <rafarizal423@gmail.com>
2025-10-15 22:19:48 +0700

    Implement REST API with JSON response for Book and Author

---
+++ routes/api.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\GenreController;
+use App\Http\Controllers\AuthorController;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::get('/books', [BookController::class, 'index']);
+Route::get('/genres', [GenreController::class, 'index']);
+Route::get('/authors', [AuthorController::class, 'index']);
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookController;
+
 
 Route::get('/', function () {
     return view('welcome');
